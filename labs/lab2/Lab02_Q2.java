802f8d2d71db868a27a72f8a4f411d8b3eff9e38
a-erhan <ahmeterhanavci@gmail.com>
2025-10-02 10:59:12 +0300

    Update Lab02_Q2.java

--- labs/lab2/Lab02_Q2.java
+++ labs/lab2/Lab02_Q2.java
@@ -3,9 +3,9 @@ public class Lab02_Q2 {
     public static void main( String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the mass of the first object(kg): "); 
-        int mass_1= sc.nextInt();
+        double mass_1= sc.nextInt();
         System.out.print("Enter the mass of the second object(kg): "); 
-        int mass_2= sc.nextInt();
+        double mass_2= sc.nextInt();
         
         final double GRAVSTANDART = 9.80665;
         final double GRAV10 = 9.77594;
@@ -17,7 +17,7 @@ public class Lab02_Q2 {
         double gravOne100 = GRAV100 * mass_1;
         double gravOne1000 = GRAV1000 * mass_1;
         double gravTwoStandart = GRAVSTANDART * mass_2;
-        double gravTwo10 = GRA10 * mass_2;
+        double gravTwo10 = GRAV10 * mass_2;
         double gravTwo100 = GRAV100 * mass_2;
         double gravTwo1000 = GRAV1000 * mass_2;
         
@@ -25,8 +25,8 @@ public class Lab02_Q2 {
 
 
         System.out.printf("%41dkm\t%dkm\t%dkm\t%dkm %n",0,10,100,1000);
-        System.out.printf("OBJECT ONE(%f)\t%f\t%f\t%f\t%f",gravOneStandart,gravOne10,gravOne100,gravOne1000);
-        System.out.printf("OBJECT ONE(%f)\t%f\t%f\t%f\t%f",gravTwoStandart,gravTwo10,gravTwo100,gravTwo1000);
+        System.out.printf("OBJECT ONE(%f)\t%21.1f\t%.1f\t%.1f\t%.1f%n",mass_1,gravOneStandart,gravOne10,gravOne100,gravOne1000);
+        System.out.printf("OBJECT ONE(%f)\t%21.1f\t%.1f\t%.1f\t%.1f%n",mass_2,gravTwoStandart,gravTwo10,gravTwo100,gravTwo1000);
 
 
 
